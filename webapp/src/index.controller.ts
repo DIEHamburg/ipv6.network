60dab18f3be13e4b06380acee288ba40ae10ebf0
Florian Schulz <florian.schulz404@gmail.com>
2024-06-14 16:14:10 +0200

    ✨ added webapp direction

---
+++ webapp/src/index.controller.ts
@@ -0,0 +1,14 @@
+namespace Index {
+    _onInit();
+    
+    /**
+     * @private
+     * @method inits the page
+     * @author DerEineFlow
+     * @memberof AW_Crew
+     * @date 05.12.2023
+     */
+    function _onInit () {
+        // AW.Base.loading(false);
+    }
+}
